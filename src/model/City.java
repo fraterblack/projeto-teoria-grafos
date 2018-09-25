863666b1dd3aa4c3fd39ba24183389da9aa4d72a
gabrielfernandes320 <42222664+gabrielfernandes320@users.noreply.github.com>
2018-09-25 20:08:17 -0300

    update of cities model

--- src/model/City.java
+++ src/model/City.java
@@ -1,5 +1,34 @@
 package model;
 
-public class City {
-
+public class City extends ModelBase {
+	int cityId;
+	String CityName;
+	double distance;
+	
+	public int getCityId() {
+		return cityId;
+	}
+	public void setCityId(int cityId) {
+		this.cityId = cityId;
+	}
+	public String getCityName() {
+		return CityName;
+	},
+	public void setCityName(String cityName) {
+		CityName = cityName;
+	}
+	public double getDistance() {
+		return distance;
+	}
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+	
+	public City(int cityId, String cityName, double distance) {
+		super();
+		this.cityId = cityId;
+		CityName = cityName;
+		this.distance = distance;
+	}
+	
 }
