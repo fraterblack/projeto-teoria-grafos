cb8c6de71615b5d72544a6640050e690c0fa4d87
RogerVieiraa <rogeramorinvieiraa@gmail.com>
2018-09-25 19:34:47 -0300

    vai la black

---
+++ src/controller/FileController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import java.io.File;
+
+public class FileController {
+	
+	public static void moveFile(File file) {
+		
+		System.out.println("test");
+		
+		return;
+	}
+	
+	
+}
--- src/view/MainView.java
+++ src/view/MainView.java
@@ -3,7 +3,7 @@ package view;
 public class MainView {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
