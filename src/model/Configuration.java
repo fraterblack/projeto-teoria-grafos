783f71cf8cc35630e30acea65108a1e23aba03af
comp16 <comp16@lab.unesc.net>
2018-09-25 20:01:33 -0300

    modelei as classes

---
+++ src/model/Configuration.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Configuration {
+	String folder;
+	String sucess;
+	String error;
+	public String getFolder() {
+		return folder;
+	}
+	public void setFolder(String folder) {
+		this.folder = folder;
+	}
+	public String getSucess() {
+		return sucess;
+	}
+	public void setSucess(String sucess) {
+		this.sucess = sucess;
+	}
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	
+	public Configuration(String folder, String sucess, String error) {
+		super();
+		this.folder = folder;
+		this.sucess = sucess;
+		this.error = error;
+	}
+	
+	
+}
