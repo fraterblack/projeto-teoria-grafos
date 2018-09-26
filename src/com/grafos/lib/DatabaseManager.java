37fb7cabb88e136b62beee067775c5cc939d912e
rogervieiraa <rogeramorinvieiraa@gmail.com>
2018-09-26 16:43:08 -0300

    create and get config done

--- src/com/grafos/lib/DatabaseManager.java
+++ src/com/grafos/lib/DatabaseManager.java
@@ -1,7 +1,10 @@
 package com.grafos.lib;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
@@ -12,33 +15,84 @@ import com.grafos.model.Configuration;
 
 public class DatabaseManager {
 	
+	File FILECONFIG = new File(System.getProperty("user.home")+"/menorcaminhoconfig.txt");
+	
 	public void createConfig(Configuration config) {
-		File fileConfig = new File(System.getProperty("user.home")+"/menorcaminhoconfig.txt");
+		
 		
     	BufferedWriter buffWrite;
 		try {
-			buffWrite = new BufferedWriter(new FileWriter(fileConfig));
+			buffWrite = new BufferedWriter(new FileWriter(FILECONFIG));
 			String line = "";
 	        
 	        line += config.getFolder() + "\n";
 	        line += config.getSucess() + "\n";
 	        line += config.getError() + "\n";
+	        line += config.getAutomatic() + "\n";
+
 	        //criar integridade dos arquivos
 	        line += createHash(config);
 			
 	    	buffWrite.append(line + "\n");
 	        buffWrite.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
         
 	}
 	
+	public Configuration getConfig() throws IOException {
+		
+    	BufferedReader buffRead;
+		try {
+			buffRead = new BufferedReader(new FileReader(FILECONFIG));
+			String line = "";
+			line = buffRead.readLine();
+			String[] configs = new String[10];
+			int i=0;
+			while (line != null) {
+				
+				if(i > 4) {
+			    	throw new Exception("Arquivo de configuração está em formato errado.");
+			    }
+				configs[i] = line;
+			    line = buffRead.readLine();
+			    i++;
+			    
+			}
+
+			
+			System.out.println(i);
+			if(i != 5) {
+				buffRead.close();
+				throw new Exception("Arquivo de configuração incompleto.");
+			}
+			else {
+				
+				Configuration c = new Configuration(configs[0],configs[1],configs[2],configs[3].charAt(0) == 't');
+				
+				if(!createHash(c).equals(configs[4])) {
+					buffRead.close();
+					throw new Exception("Arquivo de configuração com integridade comprometida.");
+				}
+				else {
+					buffRead.close();
+					return c;
+				}
+			}
+			
+		} catch (Exception e) {
+			System.out.println("ERRO: "+e.getMessage());
+		}
+		
+		return null;
+	}
+	
 	//gera um hash em md5 com as configuracoes
 	public String createHash(Configuration config) {
 		try {
-			String s= config.getFolder()+config.getSucess()+config.getError();
+			String s= config.getFolder()+config.getSucess()+config.getError()+config.getAutomatic();
 	        MessageDigest m;
 			m = MessageDigest.getInstance("MD5");
 			m.update(s.getBytes(),0,s.length());
--- src/tester.java
+++ src/tester.java
@@ -6,7 +6,13 @@ public class tester {
 	public static void main(String[] args) {
 		DatabaseManager dm = new DatabaseManager();
 		dm.createConfig(new Configuration("folder","sucess","error",false));
-
+		try {
+			Configuration c = dm.getConfig();
+			System.out.println(c.getAutomatic());
+		} catch(Exception e) {
+			
+		}
+		
 	}
 
 }
