3eff9e02fea6a48b3a96f20b605c819e5714d4bc
Edvaldo da Rosa <fraterblack@bck.com.br>
2018-10-28 21:39:55 -0300

    Refatoração Dijsktra

--- src/com/grafos/algorithm/Dijkstra.java
+++ src/com/grafos/algorithm/Dijkstra.java
@@ -1,142 +1,165 @@
 package com.grafos.algorithm;
 
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class Dijkstra extends Graph {
-	private int distances[];
-
-	// Results
-	private int distanceToDestination = 0;
-	private TreeMap<Integer, Edge> pathToDestination;
-
-	// Fila de prioridade.
-	private HashSet<Integer> edgesRemaining = new HashSet<Integer>();
-
+	//Results
+	private int distanceToDestination;
+	private TreeMap<Integer, Edge> pathToDestination = new TreeMap<Integer, Edge>();;
+	private TreeMap<Integer, String> processingLog;
+	
 	public Dijkstra(int vertexQuantity) throws Exception {
 		super(vertexQuantity);
-
-		// Define o tamanho das distancias.
-		distances = new int[vertexQuantity];
 	}
+	
+	public void findSmallestPath(int origin, int destination) throws Exception {
+		try {
+			//Reseta o status do último processamento
+			resetProcessingState();
+			
+			//Validate origin and destination
+			if (origin < 0) {
+				throw new Exception("Origem deve ser maior ou igual a zero");
+			}
+			
+			if (destination >= getNodesQuantity()) {
+				throw new Exception("Destino deve ser menor ou igual a " + (getNodesQuantity() - 1));
+			}
+			
+			TreeMap<Integer, Integer> unsolvedNodes = new TreeMap<Integer, Integer>();
+			TreeMap<Integer, Integer> solvedNodes = new TreeMap<Integer, Integer>();
+			TreeMap<Integer, Edge> processedPaths = new TreeMap<Integer, Edge>();
+			
+			//Cria uma cópia da matriz, uma vez que a mesma será manipulada
+			Integer[][] matrix = getMatrix();
+			
+			for (int i = 0; i < getNodesQuantity(); i++) {
+				//Cria o vetor de nós restantes
+				unsolvedNodes.put(unsolvedNodes.size(), new Integer(i));
+			}
 
-	/**
-	 * Método responsável por encontrar o menor caminho.
-	 * 
-	 * @param origin
-	 * @param destin
-	 * @throws Exception
-	 */
-	public void findSmallestPath(int origin, int destin) throws Exception {
-		if (origin < 0) {
-			throw new Exception("Nó de origem não pode ser negativa");
+			//Move nó de origem no map de resolvidos
+			solvedNodes.put(origin, 0);
+			unsolvedNodes.remove(new Integer(origin));
+
+			//Enquanto houver nós não resolvidos
+			while (!unsolvedNodes.isEmpty()) {
+				//Variáveis de controle
+				int smallestResolvedNode = -1;
+				int smallestAdjacentNode = -1;
+				int smallestAdjacentValue = 0;
+				
+				for (Map.Entry<Integer, Integer> entry : solvedNodes.entrySet()) {
+					Integer currentNode = entry.getKey();
+					Integer acummulatedDistance = entry.getValue();
+					
+					//Logging
+					addLog("Itera sobre nó resolvido [" + currentNode + "]");
+					
+					//Apartir do nó resolvido (linha na matriz), itera sobre os nós adjacentes (colunas na matriz)
+					for (int i = 0; i < getNodesQuantity(); i++) {
+						//Só considera nós com valor maior que zero e que não seja adjascente a uma nó já resolvido
+						//E nós que ainda não tenham sido resolvidos
+						if (matrix[currentNode][i] > 0 && solvedNodes.get(i) == null) {
+							//Soma do peso do nó adjascente ao valor acumulado do nó resolvido
+							int adjacentDistance = matrix[currentNode][i] + acummulatedDistance;
+							
+							//Logging
+							String logMessage = "  Nó adjascente [" + i + "] - Distância: " + adjacentDistance + "(" + acummulatedDistance + ")";
+							
+							//Se for a primeira iteração
+							if (smallestAdjacentNode == -1 || adjacentDistance <= smallestAdjacentValue) {
+								smallestResolvedNode = currentNode;
+								smallestAdjacentNode = i;
+								smallestAdjacentValue = adjacentDistance;
+								
+								//Logging
+								logMessage += " ***"; //Identifica o nó adjascente como o de menor caminho até o momento
+							}
+							
+							//Logging
+							addLog(logMessage);
+						}
+					}
+				}
+				
+				//Logging
+				addLog("[Menor caminho: " + smallestResolvedNode + "->" + smallestAdjacentNode + " = " + smallestAdjacentValue + "]");
+				
+				//Adiciona o caminho para lista de processados
+				processedPaths.put(processedPaths.size(), new Edge(smallestResolvedNode, smallestAdjacentNode, smallestAdjacentValue));
+				
+				//Move menor nó adjacente dos não resolvidos para os resolvidos
+				solvedNodes.put(smallestAdjacentNode, smallestAdjacentValue);
+				unsolvedNodes.remove(new Integer(smallestAdjacentNode));
+				
+				addLog(".........................................................");
+				
+				//Menor caminho possível encontrado
+				if (smallestAdjacentNode == destination) {
+					//Logging
+					addLog("----- Menor caminho encontrado: " + smallestAdjacentValue + " -----");
+					
+					distanceToDestination = smallestAdjacentValue;
+					
+					break;
+				}
+			}
+			
+			generatePathToDestination(processedPaths);
+		} catch (Exception error) {
+			if (error.getMessage().equals("-1")) {
+				throw new Exception("Não existe um caminho viável entre os pontos");
+			}
+			
+			throw new Exception(error.getMessage());
 		}
-
-		if (destin >= getNodesQuantity()) {
-			throw new Exception("Nó de destino não pode ser maior que " + (getNodesQuantity() - 1));
-		}
-
-		initSearchForSmallestPath(origin);
-
-		pathToDestination = extractSmallestPathTo(origin, destin);
-
-		distanceToDestination = distances[destin];
 	}
-
-	public Integer getDistanceToDestination() {
-		return distanceToDestination;
-	}
-
+	
 	public TreeMap<Integer, Edge> getPathToDestination() {
 		return pathToDestination;
 	}
-
-	/**
-	 * Inicia a busca pelo menor caminho.
-	 * 
-	 * @param origin
-	 */
-	private void initSearchForSmallestPath(int origin) {
-		pathToDestination = null;
+	
+	public Integer getDistanceToDestination() {
+		return distanceToDestination;
+	}
+	
+	public void printLog() {
+		processingLog.forEach((index, log) -> System.out.println(log));
+	}
+	
+	private void resetProcessingState() {
+		processingLog = new TreeMap<Integer, String>();
+		
 		distanceToDestination = 0;
-
-		for (int i = 0; i < getNodesQuantity(); i++) {
-			distances[i] = Integer.MAX_VALUE; // Define-se a distancia de todos os nós como uma distancia infinita.
-
-			edgesRemaining.add(new Integer(i)); // adiciona a aresta na fila
-		}
-
-		distances[origin] = 0; // Define a distância da origem como ZERO.
+		pathToDestination.clear();
 	}
-
-	/**
-	 * Extrai o menor caminho a ser seguido.
-	 * 
-	 * @return TreeMap<Integer, Path>
-	 */
-	private TreeMap<Integer, Edge> extractSmallestPathTo(int origin, int destin) throws Exception {
-		TreeMap<Integer, Edge> paths = new TreeMap<Integer, Edge>();
-
-		boolean smallestPathFound = false;
-		int nodeOrigin = origin;
-		int lastValue = 0;
-		// Percorre a lista de prioridade.
-		while (!edgesRemaining.isEmpty()) {
-			boolean change = false;
-			int smallValue = Integer.MAX_VALUE;
-			int nodeWithSmallestWeight = 0;
-			
-			Iterator<Integer> it = edgesRemaining.iterator();
-			while (it.hasNext()) {
-				int currentVertex = it.next();
+	
+	private void generatePathToDestination(TreeMap<Integer, Edge> processedPaths) {
+		int lastOriginToFound = -1;
+		for (Entry<Integer, Edge> entry : processedPaths.entrySet().stream()
+				.sorted((p1, p2) -> p2.getKey().compareTo(p1.getKey()))
+				.collect(Collectors.toList())
+		) {
+			if (lastOriginToFound == -1) {
+				lastOriginToFound = entry.getValue().getNodeOrigin();
 				
-				if (distances[currentVertex] < smallValue) {
-					smallValue = distances[currentVertex];
-					nodeWithSmallestWeight = currentVertex;
-					change = true;
+				pathToDestination.put(entry.getKey(), entry.getValue());
+			} else {
+				if (entry.getValue().getNodeDestin() == lastOriginToFound) {
+					lastOriginToFound = entry.getValue().getNodeOrigin();
+					
+					pathToDestination.put(entry.getKey(), entry.getValue());
 				}
 			}
-			
-			// Se o menor caminho ainda não foi encontrado
-			if (!smallestPathFound && smallValue > lastValue) {
-				paths.put(paths.size(), new Edge(nodeOrigin, nodeWithSmallestWeight, smallValue));
-
-				// variável de controler para saber o último vértice na próxima iteração
-				nodeOrigin = nodeWithSmallestWeight;
-			}
-
-			// Se o nó destino for o mesmo nó com menor menor caminho, o menor caminho foi
-			// encontrado
-			if (destin == nodeWithSmallestWeight) {
-				smallestPathFound = true;
-			}
-			
-			edgesRemaining.remove(new Integer(nodeWithSmallestWeight));
-
-			// Percorre todos os itens restantes para relaxar os itens
-			for (int i = 0; i < getNodesQuantity(); i++) {
-				if (getMatrix()[nodeWithSmallestWeight][i] > 0) {
-					calculateDistances(nodeWithSmallestWeight, i);
-				}
-			}
-			
-			if(!change) {
-				throw new Exception("Caminho impossivel.");
-			}
-			lastValue = smallValue;
 		}
-
-		return paths;
 	}
-
-	/*
-	 * Relaxa arestas no grafo
-	 */
-	private void calculateDistances(int i, int j) {
-		if (distances[j] > distances[i] + getMatrix()[i][j]) {
-			distances[j] = distances[i] + getMatrix()[i][j];
-		}
+	
+	private void addLog(String message) {
+		processingLog.put(processingLog.size(), message);
 	}
 
 	public static void main(String[] args) {
@@ -181,30 +204,42 @@ public class Dijkstra extends Graph {
 			System.out.println("########### Test Case ##########");
 			
 			//Case 1
-			dij.findSmallestPath(6, 7);
+			dij.findSmallestPath(0, 7);
 			
+			System.out.println("Rota:");
 			dij.getPathToDestination().forEach((key, edge) -> {
 				System.out.println(edge.getNodeOrigin() + "->" + edge.getNodeDestin() + "=" + edge.getAccumulatedDistance());
 			});
 
-			System.out.println(dij.getDistanceToDestination()); //Esperado: Total 10
-			System.out.println("test");
+			System.out.println("Menor distância:");
+			System.out.println(dij.getDistanceToDestination());
+			
+			System.out.println("Log:");
+			dij.printLog();
+			
+			System.out.println("#");
+			System.out.println("########### Test Case 2 ##########");
 			///////
 			
 			//Case 2
 			dij.findSmallestPath(3, 5);
 			
+			System.out.println("Rota:");
 			dij.getPathToDestination().forEach((key, edge) -> {
 				System.out.println(edge.getNodeOrigin() + "->" + edge.getNodeDestin() + "=" + edge.getAccumulatedDistance());
 			});
 
-			System.out.println(dij.getDistanceToDestination()); //Esperado: Total 5
+			System.out.println("Menor distância:");
+			System.out.println(dij.getDistanceToDestination());
+			
+			System.out.println("Log:");
+			dij.printLog();
 			///////
 		} catch (Exception ex) {
 			if (ex.getMessage() == null)
 				System.out.println("Ocorreu um erro de " + ex + " no main");
 			else
-				System.out.println(ex.getMessage() + "XXX");
+				System.out.println("Erro: " + ex.getMessage());
 		}
 
 	}
