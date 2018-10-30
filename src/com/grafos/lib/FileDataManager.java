b13229ef7b31db397107016aad4c04372d7e3eff
Edvaldo da Rosa <fraterblack@bck.com.br>
2018-10-30 19:35:58 -0300

    Some small adjusts

--- src/com/grafos/lib/FileDataManager.java
+++ src/com/grafos/lib/FileDataManager.java
@@ -12,25 +12,34 @@ public class FileDataManager {
 	
 	public static TreeMap<Integer, String[]> extractFileData(String filePath) throws Exception {
 		TreeMap<Integer, String[]> extractedData = new TreeMap<Integer, String[]>();
+		BufferedReader bufferArquivo = new BufferedReader(new FileReader(filePath));
 		
 		try {
-			BufferedReader bufferArquivo = new BufferedReader(new FileReader(filePath));
-	
 			String lineData = bufferArquivo.readLine();
 			
 			while (lineData != null) {
 				if (!lineData.isEmpty()) {
 					String[] rowData = lineData.split(DATA_SEPARATOR);
 					
+					if (rowData.length != 5) {
+						throw new Exception("Dados corrompidos.");
+					}
+					
+					for (String data: rowData) {
+						if (data.isEmpty()) {
+							throw new Exception("O arquivo contém dados faltantes.");
+						}
+					}
+					
 					extractedData.put(extractedData.size(), rowData);
 				}
 				
 				lineData = bufferArquivo.readLine();
 			}
-			
-			bufferArquivo.close();
 		} catch(Exception error) {
 			throw new Exception("Arquivo inválido: " + error.getMessage());
+		} finally {
+			bufferArquivo.close();
 		}
 		
 		return extractedData;
